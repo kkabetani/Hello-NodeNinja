9d05290a3ee40163ad21d5ae35104035612dfedd
kazhr <k.kazhr@gmail.com>
2012-06-10 01:27:25 +0900

    Initial Hello-NodeNinja

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end(process.argv[2]);
+}).listen(process.env.listenport);
