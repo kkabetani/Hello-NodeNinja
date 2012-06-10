a79ec965311636d31012a71b285eae23ac1f0f3e
kazhr <k.kazhr@gmail.com>
2012-06-10 22:52:15 +0900

    add charset

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   response.end('Hello NodeNinja!!更新した');
 }).listen(8080);
