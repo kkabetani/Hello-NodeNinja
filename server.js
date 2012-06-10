27cd61a4d293395a89fb73543a09932e4aaab609
kazhr <k.kazhr@gmail.com>
2012-06-10 16:27:21 +0900

    メッセージ、ポートを固定

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end(process.argv[2]);
-}).listen(process.env.listenport);
+  response.end('Hello NodeNinja!!');
+}).listen(8080);
